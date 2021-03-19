ecb68606d9562e7ab0f323f12a2ebc96db704ad8
Adarsh TS <tsadarsh0707@gmail.com>
2021-03-19 11:35:59 +0530

    login error exits script;

--- kill.sh
+++ kill.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+base_url="-# http://localhost:9515/session/$sid"
+log=/dev/null
+
 echo -e "\nExiting..."
 pid=$(netstat -tulpn | grep -e "LISTEN.*chromedrive" -o | cut -d'N' -f2 | cut -d'/' -f 1 | uniq | xargs)
-curl -X DELETE $base_url
+curl -X DELETE $base_url > $log
 kill $pid
 exit
